a2c3b44652473faa557d9ffd16c4c56c0e2428c6
PM2496 <2496288505@qq.com>
2023-04-29 00:35:40 +0800

    some database test

---
+++ createRootUser.py
@@ -0,0 +1,22 @@
+import pymysql
+from pymysql.constants import CLIENT
+
+conn = pymysql.connect(host='124.71.219.185', user='root', password='uestc2022!',
+                       database='cs2329.his', charset='utf8',
+                       client_flag=CLIENT.MULTI_STATEMENTS)
+
+cursor = conn.cursor()
+# 创建管理员账户表
+# sql = "create table ROOT(USERNAME CHAR(20) not null , PASSWORD CHAR(20) not null, primary key (USERNAME))"
+sql = "insert into root values('root', '909090')"
+
+try:
+    cursor.execute(sql)
+    conn.commit()
+    print('插入成功')
+except:
+    conn.rollback()
+    print('插入失败')
+cursor.close()
+conn.close()
+
--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from PySide2.QtWidgets import QApplication, QMessageBox
+from PySide2.QtUiTools import QUiLoader
 import pymysql
+from pymysql.constants import CLIENT
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Win_Login:
+    def __init__(self):
+        self.ui = QUiLoader().load('login.ui')
+
+        self.ui.btn_login.clicked.connect(self.onSignIn)
+        self.ui.edt_password.returnPressed.connect(self.onSignIn)
+
+    def onSignIn(self):
+        if not self.ui.edt_username.text() or not self.ui.edt_password.text():
+            print('请输入用户名和密码')
+            return
+
+        conn = pymysql.connect(host='124.71.219.185', user='root', password='uestc2022!',
+                               database='cs2329.his', charset='utf8',
+                               client_flag=CLIENT.MULTI_STATEMENTS)
+        cursor = conn.cursor()
+        print(self.ui.edt_username.text())
+        print(self.ui.edt_password.text())
+        # 注意这里的%s外要加上‘’， 否则会报错
+        sql = "select * from ROOT where USERNAME='%s' and PASSWORD='%s'"%(self.ui.edt_username.text(), self.ui.edt_password.text())
+
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            username = row[0]
+            password = row[1]
+            print("username=%s, password=%s"%(username, password))
+
+        conn.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = QApplication([])
+stats = Win_Login()
+stats.ui.show()
+app.exec_()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
